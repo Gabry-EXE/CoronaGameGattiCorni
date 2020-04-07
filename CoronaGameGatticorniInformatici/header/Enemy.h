c591a30b18f4798ea1642fba24f7efa634416321
NicolaVivante <57539526+NicolaVivante@users.noreply.github.com>
2020-04-07 10:36:11 +0200

    finished enemy

--- CoronaGameGatticorniInformatici/header/Enemy.h
+++ CoronaGameGatticorniInformatici/header/Enemy.h
@@ -1,6 +1,23 @@
 #ifndef ENEMY_H
 #define ENEMY_H
 
-int blabla();
+#include "Output.h"
+
+
+class Enemy
+{
+    public:
+        Enemy(const int &x);
+
+        bool isColliding;
+
+//        void Start(const int &x, const int &y);
+        void Update();
+
+    private:
+        float m_timer;
+        COORD m_pos;
+        void Move();
+};
 
 #endif // ENEMY_H
