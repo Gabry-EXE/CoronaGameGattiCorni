a5a7f60110d6803643237a4488dd99d6ee7aac67
Luca <57528364+LucaVaccari@users.noreply.github.com>
2020-04-15 07:46:21 +0200

    Comments on main.cpp

--- CoronaGameGatticorniInformatici/main.cpp
+++ CoronaGameGatticorniInformatici/main.cpp
@@ -22,6 +22,7 @@ int main()
     Output::Get().Start();
     Player::Get().Start();
 
+    //the player is on another thread because it waits for input, blocking his thread
     std::thread playerThread(PlayerThread);
 
     while(inGame)
@@ -31,18 +32,23 @@ int main()
     }
 
     //END GAME
+    //called manually because when the game finishes it would stops printing
     Output::Get().Update();
+    
     //wait for player input before printing game stats
-    playerThread.join();
+    playerThread.join(); //join waits until the thread stops
 
+    //clear the string
     Output::Get().FillChar({ 0, 0}, { SCREEN_WIDTH, SCREEN_HEIGHT }, ' ', BACKGROUND_PURPLE);
 
+    //pick a random phrase to be displayed
     const std::string losePhrases[] = { "You're a loser", "You lost", "GG", "LOL, you lost", ":(", "You got corona", "Fatal error", "Execution killed" };
     std::string losePhrase = losePhrases[rand() % 8];
     Output::Get().PrintString( SCREEN_HEIGHT / 3, losePhrase, FOREGROUND_YELLOW | BACKGROUND_PURPLE);
 
 
     Output::Get().Update();
+    //until the player doesn't press ESCAPE, the game doesn't close
     while(!Input::Get().IsPressed(VK_ESCAPE)) {}
 
     return 0;
